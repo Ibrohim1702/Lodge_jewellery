51d20a1c5d0a0e203a4e4f2d6c3e36e5fdea9ad1
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-07-27 16:27:16 +0500

    About. Email
    About. Email

--- sayt/views.py
+++ sayt/views.py
@@ -41,9 +41,15 @@ def contact(requests):
 
 
 def about(requests):
-    ctx = {
-
-    }
+    ctx = {}
+    if requests.POST:
+        email = requests.POST.get('email')
+        Subscribe.objects.create(
+            email=email
+        )
+        ctx = {
+            "email": email
+        }
     return render(requests, "about.html", ctx)
 
 
